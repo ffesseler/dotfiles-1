95491ea11c13ff7934e6fafbab64924c7b58d0c3
Dan Shearmur <dan.shearmur@gmail.com>
2014-07-16 12:19:34 +0100

    remove unused stuff

--- aliases.sh
+++ aliases.sh
@@ -1,25 +1,7 @@
 
 alias less=$PAGER
 alias zless=$PAGER
-alias qm="mvn -DskipTests -DskipITs -Dmaven.test.skip"
 alias tmux="TERM=screen-256color-bce tmux"
-
-pull-mvn () {
-    PULL=$(git pull)
-    if [ $PULL != "Already up-to-date." ]
-        then
-            qm
-    fi
-}
-
-sipp2 () {
-    PWD=$(pwd)
-    cd ~/src/sipp2/sipp2-parent
-    pull-mvn
-    cd ~/src/sipp2/sipp2-core
-    pull-mvn
-    cd $PWD
-}
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 	alias ls="command ls -G"
