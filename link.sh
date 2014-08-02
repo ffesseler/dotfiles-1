e54e451e977e238f628ad499ed244b4918442836
Dan Shearmur <dan.shearmur@gmail.com>
2014-08-02 13:52:31 +0100

    Use symlinks for all dotfiles

--- link.sh
+++ link.sh
@@ -3,17 +3,9 @@
 for filename in .*; do
   if [ $filename != ".git" ] && [ $filename != ".gitmodules" ]  && [ $filename != "." ] && [ $filename != ".." ]
   then
-    if [ -d $filename ]
+    if [ ! -d ~/$filename ] && [ ! -f ~/$filename ]
     then
-      if [ ! -d ~/$filename ]
-      then
-        ln -s ~/.dotfiles/$filename ~/$filename
-      fi
-    else
-      if [ ! -f ~/$filename ]
-      then
-        ln $filename ..
-      fi
+      ln -s ~/.dotfiles/$filename ~/$filename
     fi
   fi
 done
