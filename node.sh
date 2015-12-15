c975840db6e04d04bb64462973d10ae07e030314
Dan Shearmur <dan.shearmur@gmail.com>
2015-12-15 08:39:43 +0000

    add n

--- node.sh
+++ node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 npm update -g
-npm install -g grunt-cli bower jshint generator-webapp generator-angular generator-react
+npm install -g grunt-cli bower jshint generator-webapp generator-angular generator-react n
